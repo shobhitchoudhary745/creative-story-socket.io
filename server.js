2cc037fd1197ba10cc7def33a12e141850a95fe1
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-01-29 15:25:23 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const app = require("./app");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT||4000;
+const users  = {};
+const rooms = {};
+
+
+io.on("connection", socket => {
+  io.emit("welcome",{data:"welcome Ansh from server"});
+  console.log("socket id:",socket.id);
+  socket.on("joinRoom",async({roomId})=>{
+    try{
+    rooms[users[socket.id]] = [];
+    rooms[users[socket.id]].push(roomId);
+    socket.join(roomId);
+  }catch(err){
+      console.log(err)
+      io.to(socket.id).emit("error",err);
+    }
+  })
+  socket.on("login",async({id})=>{
+    try{
+      socket.join(id);
+      users[socket.id] = id;
+      console.log(users);
+    }catch(err){
+      console.log(err);
+      io.to(socket.id).emit("error",err);
+    }
+  })
+
+  socket.on("message",async({roomId,message,senderId})=>{
+    try {
+      io.to(roomId).emit("sendMessage",{message,senderId});
+      // io.emit("welcome",{message:"hello Ansh from server"})
+      console.log(roomId,message,senderId)
+    } catch (error) {
+      
+    }
+  })
+
+  socket.on("disconnect", async({id}) => {
+    console.log("user is disconnected: ",socket.id);
+    // for(let i of rooms[users[socket.id]]){
+    //   io.to(i).emit("user-left",{})
+    // }
+    // delete users[socket.id];
+    // console.log(users);
+  });
+});
+
+server.listen(port, () => console.log("server running on port:" + port));
